5cf417d242465420ad9143782b5383ac64433524
lilach <lilachip@yahoo.com>
2018-02-01 18:40:18 +0200

    book-list.html styling

--- src/app/book/book.service.ts
+++ src/app/book/book.service.ts
@@ -1,12 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Book } from './book';
-import {Http} from '@angular/http';
-import {Observable} from 'rxjs';
+import {Http, Response, Headers, RequestOptions} from '@angular/http';
+import {Observable} from 'rxjs/Rx';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/catch';
 
 @Injectable()
 export class BookService {
+  constructor(private _http: Http) {}
 
-  constructor(private _http: Http) { }
   addBook(book: Book) {
     return this._http.post('/books', book).map(data => data.json()).toPromise();
   }
@@ -19,6 +21,7 @@ export class BookService {
     return this._http.put('/books/' + book.id, book).map(data => data.json()).toPromise();
    }
 // :Observable<Book>
+// '/src/books-mock.json'
   getBooks() {
     return this._http.get('/books').map(data => data.json()).toPromise();
    }
