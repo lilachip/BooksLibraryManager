e7f4a1ea8c5216969f41b529f3768567c4767e80
lilach <lilachip@yahoo.com>
2018-03-05 14:09:30 +0200

    HTML fixups

--- src/app/book/book.service.ts
+++ src/app/book/book.service.ts
@@ -26,36 +26,12 @@ export class BookService {
     this.addedBooks.push(newbook);
     return newbook;
   }
-  /*addBooks = function(data){
-    let newBook = {
-        "id":this.book.id,
-        "title":this.book.Title,
-        "author":this.book.Author,
-        "date":this.book.Date
-     };
-    this.books.push(newBook);
-    }*/
-
-  /*public selectAllBooks() {
-    return this.books;
-  }*/
 
   public waitForBooksList(action: Function) { // waitForBooksList = function
     this.whenBooksListReady = action; // whenBooksListReady = delegation
 
     this.getJSON().subscribe(data => {
       this.data = data;
-      // tslint:disable-next-line:comment-format
-      //console.log(this.books);
-      // tslint:disable-next-line:comment-format
-      //this.whenBooksListReady(this.books);
-
-      // console.log(data.Books);
-      // console.log(this.books);
-      // tslint:disable-next-line:comment-format
-      //console.log(this.book);
-
-
     });
 
 
@@ -63,24 +39,11 @@ export class BookService {
 
   public getJSON(): Observable<any> {
     return this.http.get('../assets/books-mock.json');
-    // .map(res => {
-    //   return res.json().books.map(book => {
-    //     return new Book (
-    //       book.id,
-    //       book.title,
-    //       book.author,
-    //       book.date,
-    //       book.img,
-    //       book.editable
-    //     );
-    //   });
-    // });
   }
 
   getBookIndexById(book: any) {
     let index = 0;
     // tslint:disable-next-line:comment-format
-    //debugger;
     for (; index < this.data.Books.length; index++) {
       if (this.data.Books[index].Id === book.Id) {
         return index;
@@ -107,7 +70,5 @@ export class BookService {
   editBook(book: Book) {
     // return this.http.put('/books/' + book.id, book).map(data => data.json()).toPromise();
   }
-
-
 
 }
